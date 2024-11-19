353853a9ef03a420ab6392c8ae199ce8132f1bce
Kristhian De Oliveira <kristhian.deoliveira@gmail.com>
2024-11-19 11:51:38 -0500

    Initial Commit

---
+++ SuperFlicker/SuperFlickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperFlickerApp.swift
+//  SuperFlicker
+//
+//  Created by Kristhian De Oliveira on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SuperFlickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
